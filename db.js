3e556c6976230715ffce3f62fa833b7553aac49a
kunalpawar83 <kunalpawar8319@gmail.com>
2024-06-13 11:55:23 +0530

    first commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose  = require('mongoose');
 const connectDB = async () => {
     try {
-      await mongoose.connect('mongodb://localhost:27017/excel-json');
+      await mongoose.connect('mongodb+srv://kunalpawar8319:kunalmain2018@cluster0.wgaedti.mongodb.net/excel-json');
       console.log("MongoDB connected");
     } catch (error) {
       console.error("MongoDB connection error:", error);
