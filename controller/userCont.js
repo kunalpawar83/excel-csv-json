be5a73ec702278f00215873133d32dd5e778f4fa
kunalpawar83 <kunalpawar8319@gmail.com>
2024-06-14 13:28:29 +0530

    create single route for all

--- controller/userCont.js
+++ controller/userCont.js
@@ -3,13 +3,16 @@ const csv = require('csvtojson');
 const fs = require('fs').promises;
 const XLSX = require('xlsx');
 const path = require('path');
+
+
+exports.getAllData = async (req, res) => {
+    const data = await User.find();
+    res.json(data);
+    };
+    
 // Upload JSON file
-exports.addUser= async (req, res) => {
-    const file  = req.file.originalname;
-    let filename  = file.split('.')[1];
-    console.log(filename);
+exports.addUserJson= async (req, res) => {
     try {
-        if(filename == 'json'){
         // Read the uploaded JSON file
         const jsonData = await fs.readFile(req.file.path, 'utf-8');
         // Parse the JSON data
@@ -20,51 +23,12 @@ exports.addUser= async (req, res) => {
             message: 'Data uploaded successfully!',
             data: newData
         });
-        }
-        else if(filename == 'csv'){
-            csv().fromFile(req.file.path).then((jsonObj)=>{
-                const users = jsonObj.map(user => {
-                    return new User({
-                        name: user.name,
-                        email: user.email,
-                        mobile: user.mobile
-                    });
-                });
-                User.insertMany(users).then((data)=>{
-                    console.log(data);
-                }).catch((err)=>{
-                    console.log(err);
-                });
-            })
-            res.status(200).json({message: 'Data uploaded successfully'});
-        }
-        else if (filename == 'xlsx'){
-            const filePath = path.resolve(req.file.path);
-            const file = await fs.readFile(filePath);
-    
-            // Parse the Excel file
-            const workbook = XLSX.read(file, { type: 'buffer' });
-            const sheetName = workbook.SheetNames[0];
-            const sheet = workbook.Sheets[sheetName];
-            const jsonData = XLSX.utils.sheet_to_json(sheet);
-    
-            // Insert parsed data into MongoDB
-            const data = await User.insertMany(jsonData);
-            res.json({ 
-                message: 'Data uploaded successfully!',
-                data: data
-            });
-        }
       } catch (err) {
         console.error('Error uploading data:', err);
         res.status(500).json({ message: 'Error: ' + err.message });
       }
 };
 
-exports.getAllData = async (req, res) => {
-    const data = await User.find();
-    res.json(data);
-};
 
 //Upload CSV file
 exports.addUserCsv = async (req, res) => {
@@ -92,26 +56,26 @@ exports.addUserCsv = async (req, res) => {
 };
 
 // Upload Excel file
-// exports.addUserExcel = async (req, res) => {
-//     try{
-//             const filePath = path.resolve(req.file.path);
-//             const file = await fs.readFile(filePath);
+exports.addUserExcel = async (req, res) => {
+    try{
+            const filePath = path.resolve(req.file.path);
+            const file = await fs.readFile(filePath);
     
-//             // Parse the Excel file
-//             const workbook = XLSX.read(file, { type: 'buffer' });
-//             const sheetName = workbook.SheetNames[0];
-//             const sheet = workbook.Sheets[sheetName];
-//             const jsonData = XLSX.utils.sheet_to_json(sheet);
+            // Parse the Excel file
+            const workbook = XLSX.read(file, { type: 'buffer' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const jsonData = XLSX.utils.sheet_to_json(sheet);
 
-//             // Insert parsed data into MongoDB
-//             const data = await User.insertMany(jsonData);
-//             res.json({ 
-//                 message: 'Data uploaded successfully!',
-//                 data: data
-//             });
-//     }catch(err){
-//         console.log(err);
-//         res.status(500).json({message: 'Error uploading data'});
-//     }
-// };
+            // Insert parsed data into MongoDB
+            const data = await User.insertMany(jsonData);
+            res.json({ 
+                message: 'Data uploaded successfully!',
+                data: data
+            });
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: 'Error uploading data'});
+    }
+};
 
